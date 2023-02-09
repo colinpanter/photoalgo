dedf6b8bd081422277ccb03cecbe094b0e9d3895
Colin <colin.panter.1@ulaval.ca>
2023-02-09 18:03:36 -0500

    Partie 2 + Sources | TP2

--- tp2/code/main_pile.py
+++ tp2/code/main_pile.py
@@ -1,25 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage import img_as_float, img_as_ubyte
+from skimage import img_as_float
+from skimage.io import imread
 
 from stack import Stacker
 
 
 if __name__ == "__main__":
-    img = img_as_float(plt.imread("tp2/images/colympic.jpg"))
+    img = img_as_float(imread("tp2/images/colympic.jpg"))
 
     plt.figure(figsize=(16,6))
 
-    stack = Stacker(img, n_filters=5, start=1/2)
+    stack = Stacker(img, n_filters=5, start=1/4)
     length = stack.gaussian.shape[0]
     for i in range(length):
         ax = plt.subplot(2, length, i+1)
         ax.imshow(stack.gaussian[i], cmap="gray")
+        ax.set_title("Pile gaussienne" if i == 0 else None)
         ax.axis('off')
         
         ax = plt.subplot(2, length, length+i+1)
-        l = stack.laplacian[i]
-        ax.imshow((l-l.min()) / (l.max()-l.min()), cmap="gray")
+        l = np.clip(np.abs(stack.laplacian[i]), 0, 1)
+        ax.imshow(l / l.max(), cmap="gray")
+        ax.set_title("Pile laplacienne" if i == 0 else None)
         ax.axis('off')
 
     plt.tight_layout()
